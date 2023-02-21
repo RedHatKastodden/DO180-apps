55b8f9029cbb908ea89785491ae7d5d57169f3c9
Student User <student@workstation.lab.example.com>
2023-02-21 08:59:03 -0500

    updated in vim by Kastodden

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
-print "Hello, World Ellen-Elisabeth Kastodden! php version is " . PHP_VERSION .
-"\n";
-print "123458978 A brand new day is coming - enda en endring fra Kastodden!!\n";
+print "Hello, World Ellen-Elisabeth". php version . PHP_VERSION . "\n";
+print "A change is in the air!";
 ?>
