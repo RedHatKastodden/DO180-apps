d1982adbf1273764ff182ee388adfb971d8d3d88
Student User <student@workstation.lab.example.com>
2023-02-21 04:27:23 -0500

    Changes has been made

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World Ellen-Elisabeth Kastodden! php version is " . PHP_VERSION .
 "\n";
-print "A brand new day is coming!!\n";
+print "A brand new day is coming - enda en endring fra Kastodden!!\n";
 ?>
