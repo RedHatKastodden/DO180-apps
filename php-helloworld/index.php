90df73b6bad72663c1e08ee56fb9ebf12f8bad3a
Student User <student@workstation.lab.example.com>
2023-02-21 03:40:41 -0500

    Changed content of the index apage

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
-print "Hello, World Ellen-Elisabeth Kastodden! php version is " . PHP_VERSION . "\n";
+print "Hello, World Ellen-Elisabeth Kastodden! php version is " . PHP_VERSION .
+"\n";
+print "A brand new day is coming!!\n";
 ?>
